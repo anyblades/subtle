196000a43da1274daf643e506ff0dddeb70f22cf
Anton Staroverov <73071319+tonystar@users.noreply.github.com>
2026-04-27 16:07:13 -0600

    feat: add asset passthrough, update link paths, and standardize site layout with blog post list rendering

--- .subtle/eleventy.config.js
+++ .subtle/eleventy.config.js
@@ -3,12 +3,12 @@ import { readFileSync } from "node:fs";
 
 export default function (eleventyConfig) {
   eleventyConfig.setIncludesDirectory(".subtle/_includes/");
-  eleventyConfig.watchIgnores.add("../.subtle/{_site,node_modules}/");
+  baseConfig(eleventyConfig);
+
+  eleventyConfig.addPassthroughCopy("../**/*.png");
 
   //TODO: move to 11ty-blades?
   for (const line of readFileSync("./.gitignore", "utf8").split("\n"))
     if (line && !line.startsWith("#"))
       eleventyConfig.watchIgnores.add(`../.subtle/${line}`);
-
-  baseConfig(eleventyConfig);
 }
